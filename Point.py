a08b74e25fa956c4ea310ef394ffcad61cde4920
chengtao-yang <yangchengtao23009@gmail.com>
2024-09-11 15:10:33 -0400

    Add files via upload

---
+++ Point.py
@@ -0,0 +1,12 @@
+class Point:
+    def __init__(self, x, y):
+        """Initializes a 2-D point with x- and y- coordinates"""
+
+    def __eq__(self, other):
+        """ADD YOUR OWN DOCSTRING"""
+
+    def equidistant(self, other):
+        """ADD YOUR OWN DOCSTRING"""
+
+    def within(self, distance, other):
+        """ADD YOUR OWN DOCSTRING"""
